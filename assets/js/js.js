9fc0181186aa237712c61055882e975d4f377896
meziane1981 <meziane_maamar@yahoo.fr>
2022-10-20 14:31:55 +0200

    6emme commit

---
+++ assets/js/js.js
@@ -0,0 +1,20 @@
+
+        jQuery(function () {
+            jQuery('.grid-stack').gridstack({
+                width: 12,
+                alwaysShowResizeHandle: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+                cellHeight: "1px",
+            });
+        });
+ 
+        jQuery(function () {
+            jQuery('.grid-stack').gridstack({
+                width: 12,
+                alwaysShowResizeHandle: /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent),
+                cellHeight: "1px",
+            });
+        });
+        
+        (function (w,i,d,g,e,t,s) {w[d] = w[d]||[];t= i.createElement(g);
+            t.async=1;t.src=e;s=i.getElementsByTagName(g)[0];s.parentNode.insertBefore(t, s);
+        })(window, document, '_gscq','script','//widgets.getsitecontrol.com/143314/script.js');
